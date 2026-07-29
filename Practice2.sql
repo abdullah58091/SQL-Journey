52bef610a309c5ab46db0bf87906ba9e96c4b2c3
abdullah58091 <abdullahjd6@gmail.com>
2026-07-29 15:50:16 +0530

    problem practice

--- Practice2.sql
+++ Practice2.sql
@@ -271,3 +271,64 @@ mysql> SELECT CONCAT(name,'-',department)
 +----------------------------------+
 10 rows in set (0.00 sec)
 
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| college            |
+| company            |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.01 sec)
+
+mysql> USE COMPANY ;
+Database changed
+mysql> SHOW TABLES ;
++-------------------+
+| Tables_in_company |
++-------------------+
+| departments       |
+| employees         |
+| meeting           |
++-------------------+
+3 rows in set (0.00 sec)
+
+mysql> SELECT * FROM employees ;
++----+----------+------+--------+------------+---------+------------+
+| id | name     | age  | salary | department | dept_id | manager_id |
++----+----------+------+--------+------------+---------+------------+
+|  1 | Abdullah |   21 |  50000 | IT         |     101 |       NULL |
+|  2 | Iram     |   20 |  45000 | HR         |     102 |          1 |
+|  3 | Sara     |   20 |  60000 | IT         |     101 |          1 |
+|  4 | Zoya     |   25 |  70000 | Finance    |     103 |          1 |
+|  5 | Aman     |   22 |  55000 | IT         |     101 |          2 |
+|  6 | John     |   28 |  80000 | Finance    |     103 |          2 |
+|  7 | Ali      |   24 |  48000 | HR         |     102 |          3 |
+|  8 | Rohan    |   27 |  75000 | IT         |     101 |          3 |
+|  9 | David    |   26 |  52000 | Unknown    |     999 |          3 |
+| 10 | Ravi     |   45 |  34000 | HR         |     102 |          1 |
++----+----------+------+--------+------------+---------+------------+
+10 rows in set (0.00 sec)
+
+mysql> SELECT UPPER(name)
+    -> FROM employees;
++-------------+
+| UPPER(name) |
++-------------+
+| ABDULLAH    |
+| IRAM        |
+| SARA        |
+| ZOYA        |
+| AMAN        |
+| JOHN        |
+| ALI         |
+| ROHAN       |
+| DAVID       |
+| RAVI        |
++-------------+
+10 rows in set (0.00 sec)
+
+mysql>
